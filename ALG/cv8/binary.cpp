bee49a19c42fc20f17902facf4f0af82541ee1f5
TheMikerik <mchlrucka@gmail.com>
2023-04-23 18:12:02 +0200

    prubezna verze alg cv8

---
+++ ALG/cv8/binary.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+///Konstanta s retezcem vypisovanym, pokud polozka nebyla v ciselniku
+const string NOT_FOUND = "Neexistujici polozka";
+
+/**
+ * Reprezentace jedne polozky v ciselniku
+ */
+struct Item {
+    int key; ///< Celociselna hodnota, klic polozky
+    string value; ///< Nazev nebo jiny retezec popisujici polozku
+    
+    /**
+     * Definice operatoru < pro struktury Item, vysledek porovnani zavisi
+     * jen na porovnani jejich klicu.
+     */
+    bool operator<(const Item a) const{
+        return (key<a.key);
+    }
+};
+
+struct Tree{
+    Item item;
+
+    Tree* lower = NULL;
+    Tree* higher = NULL;
+};
+
+/**
+ * Nacteni vstupniho souboru ciselniku do vektoru polozek reprezentovanych
+ * strukturami Item, kazda polozka ma celociselny klic (identifikator) 
+ * a retezcovou hodnotu (nazev)
+ * V souboru se ceka kazda polozka na samostatnem radku, na zacatku je 
+ * klic mezerou oddeleny od hodnoty, ta je potom az do konce radku.
+ *
+ * @param name Jmeno souboru, ze ktereho se polozky budou nacitat
+ * @return Vektor vsech polozek nactenych ze souboru
+ */
+vector<Item> read_items(string name){
+    
+    ifstream infile; 
+    infile.open(name); 
+    vector<Item> items;
+    Item it;
+
+    if(!infile.is_open()){
+        cerr << "File could not be opened" << endl;
+        exit(-1);
+    }
+    
+    while (!infile.eof()) {
+        infile >> it.key;
+        infile.ignore(256, ' ');
+        getline(infile,it.value);
+        items.push_back(it);
+    }
+    infile.close();
+    return items;
+    
+}    
+
+/**
+ * Nacteni seznamu klicu ze souboru do vektoru celociselnych hodnot.
+ * V souboru mohou byt cisla oddelena libovolnymi bilymi znaky.
+ *
+ * @param name Jmeno souboru, ze ktereho se cisla budou nacitat
+ * @return Vektor vsech cisel nactenych ze souboru
+ */
+vector<int> read_queries(string name){
+    
+    ifstream infile; 
+    infile.open(name); 
+    vector<int> queries;
+    int i;
+
+    if(!infile.is_open()){
+        cerr << "File could not be opened" << endl;
+        exit(-1);
+    }
+    
+    while (!infile.eof()) {
+        infile >> i;
+        queries.push_back(i);
+    }
+    infile.close();
+    return queries;
+    
+}    
+
+/**
+ * Funkce, ktera pomoci binarniho vyhledavaní (neboli
+ * vyhledavani pulenim intervalu) najde v ciselniku nactenem
+ * reprezentovanem vektorem polozek typu Item tu s danym klicem
+ * a vrati jeji retezcovou hodnotu. 
+ *
+ * @param items Vektor reprezentujici ciselnik
+ * @param key Klic, ktery v ciselniku budeme hledat
+ * @return Retezcova hodnota (promenna value z polozky Item) polozky
+ *         se zadanym klicem
+ */
+string findItem(vector<Item> &items, int k) {
+    //Doplnte telo teto funkce
+    int min = 0;                // min                  12
+    int max = items.size();     // max             0 18 9, 10 18 14, 10 13 12
+    int middle;                 // middle
+
+    if ( items.empty() ){
+        return NOT_FOUND;
+    }
+    while ( min <= max ){
+        middle = (min+max)/2;
+
+        if ( items[middle].key == k){
+                return items[middle].value;
+        }
+        else{
+            if ( items[middle].key > k ){
+                max = middle-1;
+            }
+            else if ( items[middle].key < k ){
+                min = middle+1;
+            }
+        }
+    }
+    return NOT_FOUND;
+}
+
+void SearchTree(Item currentItem, Tree* tree){
+    int intree = tree->item.key;
+
+    if (currentItem.key > intree ){         // vetsi
+        if (tree->higher != NULL){
+            SearchTree(currentItem, tree->higher);
+        }
+        else {
+            tree->higher->item = currentItem;
+            return;
+        }
+    }
+    else if( currentItem.key < intree ){    // mensi
+        if (tree->lower != NULL){
+            SearchTree(currentItem, tree->lower);
+        }
+        else {
+            tree->lower->item = currentItem;
+            return;
+        }
+    }
+}
+
+void CreateSearchTree(vector<Item> &items, Tree* tree){
+    if ( items.empty() ){
+        return;
+    }
+    
+    int i = 0;
+    tree->item = items[i];
+    i++;
+    while ( i < items.size() ){
+        SearchTree(items[i], tree);
+        i++;
+    }
+
+
+};
+
+int main(int argc, char** argv)
+{
+    
+    if(argc < 3){
+        cerr << "Missing arguments" << endl;
+        exit(-1);
+    }
+    
+    vector<Item> items;
+    vector<int> queries;
+    items = read_items(argv[1]);
+    queries = read_queries(argv[2]);
+
+    /* Nadefinujte si vlastni typ Tree, pokud to nebude trida
+     * nebo nebudete mit konstruovani celeho stromu z vektrou 
+     * items reseno pomoci konstruktoru dane tridy, upravte
+     * nasledujici radek
+     */
+
+    // Tree t(items); 
+    // cout<<"Binarni vyhledavaci strom"<<endl;
+    // cout<<"-------------------------"<<endl;
+    // for (int x:queries) {
+    //     /* Misto t.search(x) na nasledujicim radku dejte volani
+    //      * vami vytvorene funkce pro vyhledavani v binarnim stromu,
+    //      * predejte ji parametr x obsahujici cele cislo k vyhledani.
+    //      */
+    //     cout << x << " - " << t.search(x) << endl;
+    // }
+
+    sort(items.begin(), items.end());
+    cout<<endl;
+    cout<<"Vyhledavani pulenim intervalu"<<endl;
+    cout<<"-----------------------------"<<endl;
+    for (int x:queries) {
+        cout << x << " - " << findItem(items, x) << endl;
+    }
+    return 1;
+}
+
+
