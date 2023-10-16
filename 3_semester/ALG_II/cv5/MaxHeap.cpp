bd77cae99d5b275e8f7a602ca1ac2fa5ad5a8fa5
TheMikerik <mchlrucka@gmail.com>
2023-10-16 15:02:56 +0200

    ALG-II cv5

--- 3_semester/ALG_II/cv5/MaxHeap.cpp
+++ 3_semester/ALG_II/cv5/MaxHeap.cpp
@@ -11,21 +11,25 @@ private:
     vector<int> heap_array;
 public:
     Heap(void) {}
+
     void swap(int *a, int *b)
     {
     int temp = *b;
     *b = *a;
     *a = temp;
     }
+
     int size(){
         return this->heap_array.size();
     }
-//    bool is_empty(void) {}
+
     void print(){
         for (int i=0; i<this->size(); i++){
-            cout << this->heap_array[i] << endl;
+            cout << this->heap_array[i] << " ";
         }
+        cout << endl;
     }
+
     void insert(int number){
         this->heap_array.push_back(number);
 
@@ -39,25 +43,27 @@ public:
                 int left = i * 2 + 1;
                 int right = i * 2 + 2;
                 
-                if(heap_array[left] > heap_array[root]){
+                if(heap_array[left] > heap_array[root] && left <= this->size()-1){
                     swap(&heap_array[root], &heap_array[left]);
                 }
                 
-                if(heap_array[right] > heap_array[root]){
+                if(heap_array[right] > heap_array[root] && right <= this->size()-1){
                     swap(&heap_array[root], &heap_array[right]);
                 }
             }
         }
 
     }
-    int remove(){
+    
+    void remove(){
         int top = 0;
-        int last = this->size()-1;
-        this->swap(&heap_array[top], &heap_array[last]);
-        this->heap_array.pop_back();
-        last = heap_array[this->size()-1];
-        this->heap_array.pop_back();
-        this->insert(last);
+        int last = size()-1;
+
+        swap(&heap_array[top], &heap_array[last]);
+        heap_array.pop_back();
+        last = heap_array[size()-1];
+        heap_array.pop_back();
+        insert(last);
     }
 };
 
@@ -66,22 +72,22 @@ int main(){
 
     heap.insert(11);
     heap.insert(3);
-    heap.insert(1);
     heap.insert(6);
+    heap.insert(1);
     heap.insert(2);
     heap.insert(12);
     heap.insert(15);
 
     heap.print();
-    cout << "\n\n";
+    cout << endl;
 
     heap.remove();
     heap.print();
-    cout << "\n\n";
+    cout << endl;
 
     heap.remove();
     heap.print();
-    cout << "\n\n";
+    cout << endl;
 
     return 0;
 }
