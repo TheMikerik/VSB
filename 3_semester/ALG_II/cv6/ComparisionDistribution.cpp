fef41161db42a5b89b2274a1d877fb4d1ebfa7fd
TheMikerik <mchlrucka@gmail.com>
2023-10-23 15:32:27 +0200

    alg-ii lab06

--- 3_semester/ALG_II/cv6/ComparisionDistribution.cpp
+++ 3_semester/ALG_II/cv6/ComparisionDistribution.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 vector<int> arrComp = {4, 5, 2, 8, 9, 3, 1};
+vector<int> arrDistr = {4, 2, 2, 8, 3, 3, 1};
 
 struct LowerCount{
     int number;
@@ -21,6 +22,7 @@ class Comparision{
         Comparision(vector<int> input_arr){
             vector<LowerCount> tmp_arr;
 
+            // Urceni kolik je mensich prvku v poli
             for(int i=0; i<input_arr.size(); i++){
                 int current = input_arr.at(i);
 
@@ -33,22 +35,22 @@ class Comparision{
                 }
             }
 
+            // Setrizeni pole podle poctu mensich hodnot
             for(int i=0; i<tmp_arr.size(); i++){
-                int max_number = 0;
+                int max = 0;
                 int max_index = 0;
                 
                 for(int j=0; j<tmp_arr.size(); j++){
-                    if(tmp_arr[j].lower_count > max_number){
+                    if(tmp_arr[j].lower_count >= max && tmp_arr[j].lower_count <= tmp_arr.size() - i - 1){
                         max_index = j;
-                        max_number = tmp_arr[j].number;
+                        max = tmp_arr[j].lower_count;
                     }
                 }
 
-                arr.push_back(tmp_arr[i]);
+                arr.push_back(tmp_arr[max_index]);
             }
 
-            reverse(arr.begin(), arr.end());
-
+            // Print
             for(int i=0; i<arr.size(); i++){
                 cout << arr.at(i).number << endl;
             }
@@ -57,7 +59,48 @@ class Comparision{
         }
 };
 
+class Distribution{
+    private:
+        int min, max;
+        vector<int> freq;
+        vector<int> result;
+
+    public:
+        Distribution(vector<int> input){
+            this->min = *min_element(input.begin(), input.end());
+            this->max = *max_element(input.begin(), input.end());
+
+            int arr_size = max-min+1;
+            this->freq.resize(arr_size, 0);
+            this->result.resize(arr_size, 0);
+
+            for(int i=0; i<freq.size(); i++){
+
+                for(int j=0; j<freq.size()-1; j++){
+                    int current = input.at(j);
+                    if(i+1 == current){
+                        freq[i]++;
+                    }
+                }
+            }
+
+            for(int i=1; i<freq.size(); i++){
+                freq[i] = freq[i-1] + freq[i];
+            }
+
+            for(int i=0; i<input.size(); i++){
+                int current = input.back();
+                input.pop_back();
+
+                freq[current-1] -= 1;
+                
+            }
+        }
+
+
+};
 
 int main(){
     Comparision c(arrComp);
+    Distribution d(arrDistr);
 }
