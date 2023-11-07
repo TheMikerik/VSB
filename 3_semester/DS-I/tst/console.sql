7d673e64633198a48a5753547442ae7953c774b9
TheMikerik <mchlrucka@gmail.com>
2023-11-07 08:08:32 +0100

    ds-i tst exc

--- 3_semester/DS-I/tst/console.sql
+++ 3_semester/DS-I/tst/console.sql
@@ -1,6 +1,8 @@
 -------------------------------------------
 --        First lecture
 -------------------------------------------
+-- https://dbedu.cs.vsb.cz/cs/Files/GetFiles/2020-2021/DS%20I/ds1_sbirka_s_resenim.pdf
+-------------------------------------------
 
 -- 7
 select *
@@ -322,3 +324,90 @@ from
 where
     co.country = 'Italy' AND
     f.title = 'Motions details'
+
+
+-- 17
+select
+    concat(c.first_name, ' ', c.last_name) as customer,
+    f.title
+from
+    customer c
+    join rental r on c.customer_id = r.customer_id
+    join inventory i on r.inventory_id = i.inventory_id
+    join film f on i.film_id = f.film_id
+    join film_actor fa on f.film_id = fa.film_id
+    join actor a on a.actor_id = fa.actor_id
+where
+    a.first_name = 'Sean' AND
+    a.last_name = 'Guiness' AND
+    r.return_date is null
+
+
+-- 18
+select
+    payment.payment_id, payment.amount, payment.payment_date
+from
+    rental
+    left join payment on rental.rental_id = payment.rental_id
+
+
+-- 19
+select
+    f.title,
+    l.name
+from
+    language l
+    left join dbo.film f on l.language_id = f.language_id
+
+-- 20
+select
+    film.title,
+    l.name,
+    ol.name
+from
+    film
+    join language l on film.language_id = l.language_id
+    left join language ol on film.original_language_id = l.language_id
+
+
+-- 21
+select
+    distinct f.title
+from
+    film f
+    left join dbo.inventory i on f.film_id = i.film_id
+    left join dbo.rental r2 on i.inventory_id = r2.inventory_id
+    left join dbo.customer c on c.customer_id = r2.customer_id
+where
+    c.first_name = 'Tim' AND
+    c.last_name = 'Cary' OR
+    f.length = 48
+
+-- 22
+select
+    film.title
+from
+    film
+    left join dbo.inventory i on film.film_id = i.film_id
+where
+    i.inventory_id is null
+
+-- 23
+select
+    distinct
+    c.first_name,
+    c.last_name
+from
+    customer c
+    join rental r on c.customer_id = r.customer_id
+    left join payment p on r.rental_id = p.rental_id
+where
+    p.payment_id is null
+
+-- 24
+SELECT
+    film.title, language.name
+FROM
+    film
+    LEFT JOIN language ON film.language_id = language.language_id AND
+    language.name LIKE 'I%'
