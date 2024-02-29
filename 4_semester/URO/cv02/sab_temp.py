1f49570d5fd0c8df4b1af2f3578d38978f3b07c1
TheMikerik <mchlrucka@gmail.com>
2024-02-29 21:19:00 +0100

    URO

---
+++ 4_semester/URO/cv02/sab_temp.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+from math import sqrt
+import tkinter.font
+import os.path
+
+class myApp:
+
+    def prevod(self, event=None):
+        v = float(self.ent_in.get())
+        print(self.dir.get())
+        self.ent_out.delete(0, END)
+        self.ent_out.insert(0, str(round(v, 2)))
+
+    def __init__(self, root):
+        root.title('Převodník teplot')
+        root.resizable(False, False)
+        root.bind('<Return>', self.prevod)    
+            
+
+        def_font = tkinter.font.nametofont("TkDefaultFont")
+        def_font.config(size=16)
+
+        self.left_frame = LabelFrame(root, text="left")
+        self.right_frame = LabelFrame(root, text="right")
+
+        self.dir = IntVar()
+        self.dir.set(1) 
+        
+        self.ent_frame = Frame(self.left_frame)
+        self.lbl_in = Label(self.ent_frame, text="Vstup")
+        self.ent_in = Entry(self.ent_frame, width=10, font = def_font)
+        self.ent_in.insert(0, '0')
+
+        scriptpart  = os.path.abspath(__file__)
+        scriptdir = os.path.dirname(scriptpart)
+        imagepath = os.path.join(scriptdir, "th.png")
+
+        self.ca = Canvas(self.right_frame, width=300, height=400)
+        self.photo = PhotoImage(file="th.png")
+        self.ca.create_image(150, 200, image=self.photo)
+
+        self.left_frame.pack(side="left", fill=Y)
+        self.right_frame.pack(side="right")
+        self.ent_frame.pack()
+        self.lbl_in.pack()  
+        self.ent_in.pack()
+
+        self.ent_in.focus_force()
+
+
+root = Tk()
+app = myApp(root)
+root.mainloop()
