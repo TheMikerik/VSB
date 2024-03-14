068710c32767718a7cd4bfb2cc33ffe6fd74c2f6
TheMikerik <mchlrucka@gmail.com>
2024-03-14 12:26:54 +0100

    URO cv02

--- 4_semester/URO/cv02/sab_temp.py
+++ 4_semester/URO/cv02/sab_temp.py
@@ -1,72 +1,78 @@
-# -*- coding: utf-8 -*-
-
-import tkinter as tk
-from tkinter import font
 from tkinter import *
 import tkinter.font
 
+
+
 class myApp:
 
+    
     def prevod(self, event=None):
-            v = float(self.ent_in.get())
-            print(self.dir.get())
-            self.ent_out.delete(0, END)
-            self.ent_out.insert(0, str(round(v, 2)))
+        v = float(self.ent_in.get())
+        if self.dir.get() == 1:
+            output = (v * 1.8) + 32
+        else:
+            output = (v - 32) * 5/9
+        self.ent_out.delete(0, END)
+        self.ent_out.insert(0, str(round(output, 2)))
+
 
     def __init__(self, root):
         root.title('Převodník teplot')
-        root.resizable(False, False)
-        root.bind('<Return>', self.prevod) 
+        root.resizable(TRUE, TRUE)
+        root.bind('<Return>', self.prevod)
 
         def_font = tkinter.font.nametofont("TkDefaultFont")
-        def_font.config(size=16)
+        def_font.config(size=30)
 
-        self.left_frame = tk.Frame(root)
-        self.right_frame = tk.Frame(root)
-        self.direction_frame = tk.Frame(self.left_frame)
-        self.entries_frame = tk.Frame(self.left_frame)
-
+        self.left_frame = Frame(root)
+        self.right_frame = Frame(root)
+        self.left_section_frame = Frame(self.left_frame)
+        
         self.dir = IntVar()
-        self.dir.set(1) 
-
-        self.ent_frame = Frame(self.left_frame)
+        self.dir.set(1)
+        
+        self.ent_frame = Frame(self.left_section_frame)
         self.lbl_in = Label(self.ent_frame, text="Vstup")
         self.ent_in = Entry(self.ent_frame, width=10, font=def_font)
         self.ent_in.insert(0, '0')
-
-        self.direction_label = tk.Label(self.direction_frame, text="Conversion Direction")
-        self.check_frame = tk.Frame(self.direction_frame)
-        self.check_1 = tk.Checkbutton(self.check_frame, text="Celsius to Fahrenheit", variable=self.direction_var, onvalue=1, offvalue=2)
-        self.check_2 = tk.Checkbutton(self.check_frame, text="Fahrenheit to Celsius", variable=self.direction_var, onvalue=2, offvalue=1)
-
-        self.info_label = tk.Label(root, text="Jan Piperek, PIP0016", fg="yellow", bg="black", font=("Times New Roman", 16))
+        
+        
+        self.output_frame = Frame(self.left_section_frame)
+        self.lbl2_in = Label(self.output_frame, text = "Vystup")
+        self.ent_out = Entry(self.output_frame, width=10, font=def_font)
+        self.ent_out.insert(0, '0')
+        
+        self.title = Label(self.left_frame, text="Vyber převodník")
+        
+        self.options = Frame(self.left_frame)
+        self.option1 = Checkbutton(self.options, text=" Celsius na Fahrenheit", variable=self.dir, onvalue=1, offvalue=2)
+        self.option2 = Checkbutton(self.options, text=" Fahrenheit na Celsius", variable=self.dir, onvalue=2, offvalue=1)
+        
+        
+        login = Label(root, text="Michal Ručka, RUC0066", fg="white", bg="black", font=("Helvetica", 20))
+        login.pack(side=BOTTOM, fill=X)
+        
 
         self.ca = Canvas(self.right_frame, width=400, height=400)
-        self.photo = tk.PhotoImage(file="th_empty.png")
+        self.photo = PhotoImage(file="th.png")
         self.ca.create_image(150, 200, image=self.photo)
-        self.rectangle = self.canvas.create_rectangle(146, 292, 152, 80, fill="yellow")
 
-        self.direction_frame.pack(side="top")
-        self.left_frame.pack(side="left", fill=tk.Y)
-        self.right_frame.pack(side="right", fill=tk.BOTH, expand=True)
-        self.entries_frame.pack(side="bottom")
+        self.left_frame.pack(side="left", fill=Y)
+        self.right_frame.pack(side="right")
+        self.left_section_frame.pack(side="bottom")
         
-        self.check_frame.pack()
-        self.direction_label.pack()
-        self.check_1.pack(side=tk.LEFT)
-        self.check_2.pack(side=tk.LEFT)
+        self.title.pack()
+        self.options.pack()
+        self.option1.pack(side=LEFT)
+        self.option2.pack(side=LEFT)
         
         self.ent_frame.pack()
         self.lbl_in.pack()
         self.ent_in.pack()
-        
-        self.left_frame.pack(side="left", fill=Y)
-        self.right_frame.pack(side="right")
-        self.ent_frame.pack()
-        self.lbl_in.pack()  
-        self.ent_in.pack()
-        
-        self.canvas.pack()
+        self.output_frame.pack()
+        self.lbl2_in.pack()
+        self.ent_out.pack()
+        self.ca.pack()
         
         self.ent_in.focus_force()
 
