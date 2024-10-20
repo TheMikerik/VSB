effafb5cff07ddd6c306c42427620f1355c9c842
TheMikerik <mchlrucka@gmail.com>
2024-10-20 16:08:27 +0200

    scenes working

--- 5_semester/ZPG/project/shaders/vertex_shader.glsl
+++ 5_semester/ZPG/project/shaders/vertex_shader.glsl
@@ -3,13 +3,11 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 uniform mat4 modelMatrix;
-uniform mat4 view;
-uniform mat4 projection;
 
 out vec3 vertexColor;
 
 void main()
 {
-    gl_Position = projection * view * modelMatrix * vec4(aPos, 1.0);
+    gl_Position = modelMatrix * vec4(aPos, 1.0);
     vertexColor = aColor;
 }
