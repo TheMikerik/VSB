63fde04325bdca048c51c36893867640cca38a75
TheMikerik <mchlrucka@gmail.com>
2024-10-23 11:53:32 +0200

    OSY

--- 5_semester/OSY/lectures/cv05/cv05.cpp
+++ 5_semester/OSY/lectures/cv05/cv05.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,89 +34,100 @@ void my_nl() {
     }
 }
 
-int main() {
-    int pipe1[2], pipe2[2], pipe3[2];
+int main(int argc, char *argv[]) {
 
-    pipe(pipe1);
-    pipe(pipe2);
-    pipe(pipe3);
+  if (argc != 2) {
+      printf("Invalid parameters: %s <grep_argument>\n", argv[0]);
+      exit(1);
+  }
 
-    if (fork() == 0) {
-        int input_file = open("names.txt", O_RDONLY);
-        if (input_file == -1) {
-            perror("Chyba při otevírání vstupního souboru");
-            exit(1);
-        }
+  std::string grep_arg = argv[1];
+  
+  int pipe1[2], pipe2[2], pipe3[2];
 
-        dup2(input_file, STDIN_FILENO);
-        dup2(pipe1[1], STDOUT_FILENO);
+  pipe(pipe1);
+  pipe(pipe2);
+  pipe(pipe3);
 
-        close(pipe1[0]);
-        close(pipe1[1]);
-        close(pipe2[0]);
-        close(pipe2[1]);
-        close(pipe3[0]);
-        close(pipe3[1]);
-        close(input_file);
+  if (fork() == 0) {
+      int input_file = open("names.txt", O_RDONLY);
+      if (input_file == -1) {
+          perror("Chyba při otevírání vstupního souboru");
+          exit(1);
+      }
 
-        my_awk();
+      dup2(input_file, STDIN_FILENO);
+      dup2(pipe1[1], STDOUT_FILENO);
 
-        exit(1);
-    }
+      close(pipe1[0]);
+      close(pipe1[1]);
+      close(pipe2[0]);
+      close(pipe2[1]);
+      close(pipe3[0]);
+      close(pipe3[1]);
+      close(input_file);
 
-    if (fork() == 0) {
-        dup2(pipe1[0], STDIN_FILENO);
-        dup2(pipe2[1], STDOUT_FILENO);
+      my_awk();
 
-        close(pipe1[0]);
-        close(pipe1[1]);
-        close(pipe2[0]);
-        close(pipe2[1]);
-        close(pipe3[0]);
-        close(pipe3[1]);
+      exit(1);
+  }
 
-        execlp("sort", "sort", NULL);
-        perror("[ERROR] Sort");
-        exit(1);
-    }
+  if (fork() == 0) {
+      dup2(pipe1[0], STDIN_FILENO);
+      dup2(pipe2[1], STDOUT_FILENO);
 
-    if (fork() == 0) {
-        dup2(pipe2[0], STDIN_FILENO);
-        dup2(pipe3[1], STDOUT_FILENO);
+      close(pipe1[0]);
+      close(pipe1[1]);
+      close(pipe2[0]);
+      close(pipe2[1]);
+      close(pipe3[0]);
+      close(pipe3[1]);
 
-        close(pipe1[0]);
-        close(pipe1[1]);
-        close(pipe2[0]);
-        close(pipe2[1]);
-        close(pipe3[0]);
-        close(pipe3[1]);
+      execlp("sort", "sort", NULL);
+      perror("[ERROR] Sort");
+      exit(1);
+  }
 
-        execlp("grep", "grep", "05 A", NULL);
-        perror("[ERROR] Grep");
-        exit(1);
-    }
-    if (fork() == 0) {
-        dup2(pipe3[0], STDIN_FILENO);
+  if (fork() == 0) {
+      dup2(pipe2[0], STDIN_FILENO);
+      dup2(pipe3[1], STDOUT_FILENO);
 
-        close(pipe1[0]);
-        close(pipe1[1]);
-        close(pipe2[0]);
-        close(pipe2[1]);
-        close(pipe3[0]);
-        close(pipe3[1]);
+      close(pipe1[0]);
+      close(pipe1[1]);
+      close(pipe2[0]);
+      close(pipe2[1]);
+      close(pipe3[0]);
+      close(pipe3[1]);
 
-        my_nl();
-        exit(1);
-    }
+      execlp("grep", "grep", grep_arg.c_str(), NULL);
+      perror("[ERROR] Grep");
+      exit(1);
+  }
+  if (fork() == 0) {
+      dup2(pipe3[0], STDIN_FILENO);
 
-    close(pipe1[0]);
-    close(pipe1[1]);
-    close(pipe2[0]);
-    close(pipe2[1]);
-    close(pipe3[0]);
-    close(pipe3[1]);
+      close(pipe1[0]);
+      close(pipe1[1]);
+      close(pipe2[0]);
+      close(pipe2[1]);
+      close(pipe3[0]);
+      close(pipe3[1]);
 
-    wait(NULL);
+      my_nl();
+      exit(1);
+  }
 
-    return 0;
+  close(pipe1[0]);
+  close(pipe1[1]);
+  close(pipe2[0]);
+  close(pipe2[1]);
+  close(pipe3[0]);
+  close(pipe3[1]);
+
+  wait(NULL);
+  wait(NULL);
+  wait(NULL);
+  wait(NULL);
+
+  return 0;
 }
