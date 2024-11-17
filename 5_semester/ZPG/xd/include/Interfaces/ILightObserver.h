8efadf8e12d0a804ee4ee82b3909af47a4bfb640
TheMikerik <mchlrucka@gmail.com>
2024-11-17 13:45:54 +0100

    sync push

--- 5_semester/ZPG/xd/include/Interfaces/ILightObserver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <glm/glm.hpp>
-
-class ILightObserver {
-public:
-    virtual ~ILightObserver() {}
-    virtual void onLightUpdate(const glm::vec3& position, const glm::vec3& color) = 0;
-};
