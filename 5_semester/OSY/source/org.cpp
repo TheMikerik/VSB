61678b76a3bf521222b8f202c1636ef66d0d352f
TheMikerik <mchlrucka@gmail.com>
2024-12-04 10:26:43 +0100

    OSY sync

--- 5_semester/OSY/source/org.cpp
+++ 5_semester/OSY/source/org.cpp
@@ -102,19 +102,28 @@ LED_Data g_led_ptc[ LED_PTC_NUM ] =
 		{ LED_PTC8_PIN, LED_PTC8_GPIO },
 };
 
+#define PWD 25
+int change = 0;
+
 // This task blink alternatively both PTAx LEDs
 void task_led_pta_blink( void *t_arg )
 {
 	uint32_t l_inx = 0;
+	
+	int ontime = PWD - change;
+	int offtime = PWD - ontime;
 
     while ( 1 )
     {
+			
     	// switch LED on
         GPIO_PinWrite( g_led_pta[ l_inx ].m_led_gpio, g_led_pta[ l_inx ].m_led_pin, 1 );
-        vTaskDelay( 200 );
+        vTaskDelay( ontime );
         // switch LED off
         GPIO_PinWrite( g_led_pta[ l_inx ].m_led_gpio, g_led_pta[ l_inx ].m_led_pin, 0 );
         // next LED
+				vTaskDelay( offtime );
+
         l_inx++;
         l_inx %= LED_PTA_NUM;
     }
--- 5_semester/OSY/source/tasks/4.cpp
+++ 5_semester/OSY/source/tasks/4.cpp
@@ -1,17 +1,22 @@
+
 void control_all_leds(bool turn_on)
 {
     TaskHandle_t handle_led_pta = xTaskGetHandle(TASK_NAME_LED_PTA);
     TaskHandle_t handle_snake_l = xTaskGetHandle(TASK_NAME_LED_SNAKE_L);
     TaskHandle_t handle_snake_r = xTaskGetHandle(TASK_NAME_LED_SNAKE_R);
-    
+
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
     if (turn_on)
     {
         if (handle_led_pta)
-            vTaskResume(handle_led_pta);
+            xTaskResumeFromISR(handle_led_pta);
         if (handle_snake_l)
-            vTaskResume(handle_snake_l);
+            xTaskResumeFromISR(handle_snake_l);
         if (handle_snake_r)
-            vTaskResume(handle_snake_r);
+            xTaskResumeFromISR(handle_snake_r);
+
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
     }
     else
     {
@@ -21,7 +26,7 @@ void control_all_leds(bool turn_on)
             vTaskSuspend(handle_snake_l);
         if (handle_snake_r)
             vTaskSuspend(handle_snake_r);
-        
+
         for (int i = 0; i < LED_PTA_NUM; i++)
         {
             GPIO_PinWrite(g_led_pta[i].m_led_gpio, g_led_pta[i].m_led_pin, 0);
