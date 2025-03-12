508af7710b2def27f21b8126466eb2917da5d191
TheMikerik <mchlrucka@gmail.com>
2025-03-12 12:46:00 +0100

    ZAO -> gitignore

---
+++ 6_semester/ZAO/template/main-3.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import mss
+from pynput.mouse import Button, Controller
+import time
+
+def main():
+    template = cv2.imread('t0.png', cv2.IMREAD_COLOR)
+    w, h = template.shape[1], template.shape[0]
+    mouse = Controller()
+    
+    scaling_factor = 2.0
+    
+    with mss.mss() as sct:
+        monitor = sct.monitors[0]
+        
+        while True:
+            
+            screenshot = sct.grab(monitor)
+            img_rgb = np.array(screenshot)
+            img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_BGRA2BGR)
+            
+            result = cv2.matchTemplate(img_bgr, template, cv2.TM_CCOEFF_NORMED)
+            threshold = 0.8
+            
+            _, max_val, _, max_loc = cv2.minMaxLoc(result)
+            
+            if max_val >= threshold:
+                center_x = max_loc[0] + w // 2
+                center_y = max_loc[1] + h // 2
+                mouse.position = (center_x / scaling_factor, center_y / scaling_factor)
+                time.sleep(0.02)
+                mouse.click(Button.left, 1)
+                time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
--- 6_semester/ZAO/template/old/main.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-import matplotlib
-matplotlib.use("WebAgg")
-
-print(cv2.__version__)
