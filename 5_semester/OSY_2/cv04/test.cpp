de7ebb03f6cac58625e0f6acb7604b79fc3b38ff
TheMikerik <mchlrucka@gmail.com>
2025-10-07 17:39:25 +0200

    4th week sync

--- 5_semester/OSY_2/cv04/test.cpp
+++ 5_semester/OSY_2/cv04/test.cpp
@@ -14,15 +14,8 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  int pipe1[ 2 ];
-  int pipe2[ 2 ];
-  int pipe3[ 2 ];
-  int pipe4[ 2 ];
-
-  pipe(pipe1);
-  pipe(pipe2);
-  pipe(pipe3);
-  pipe(pipe4);
+  int pipe1[ 2 ], pipe2[ 2 ], pipe3[ 2 ], pipe4[ 2 ];
+  pipe(pipe1); pipe(pipe2); pipe(pipe3); pipe(pipe4);
 
   int lines_count = 0;
   char buffer[256];
@@ -33,13 +26,10 @@ int main(int argc, char *argv[]) {
   if (fork() == 0){
     close(pipe1[ 1 ]);
     close(pipe2[ 0 ]);
-    close(pipe3[ 0 ]);
-    close(pipe3[ 1 ]);
-    close(pipe4[ 0 ]);
-    close(pipe4[ 1 ]);
+    close(pipe3[ 0 ]); close(pipe3[ 1 ]);
+    close(pipe4[ 0 ]); close(pipe4[ 1 ]);
     
     char p_buffer[256];
-    int tmpline = 0;
     while(read(pipe1[0], p_buffer, sizeof(p_buffer)) > 0){
       int selected_line;
       int pid1;
@@ -54,13 +44,11 @@ int main(int argc, char *argv[]) {
       while (fgets(f_buffer, sizeof(f_buffer), names) != NULL){
         if (selected_line == position){
           f_buffer[strcspn(f_buffer, "\n")] = '\0';
-          sprintf(output_buffer, "%d. %s (%d)(%d)\n", tmpline, f_buffer, pid1, getpid());
+          sprintf(output_buffer, "%s (%d)(%d)", f_buffer, pid1, getpid());
           write(pipe2[1], output_buffer, strlen(output_buffer)+1);
-          printf("%s", output_buffer);
         }
         position++;
       }
-      tmpline++;
     }
 
     close(pipe1[ 0 ]);
@@ -68,29 +56,72 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
   if (fork() == 0){
-    close(pipe1[ 0 ]);
-    close(pipe1[ 1 ]);
-    close(pipe2[ 0 ]);
+    close(pipe1[ 0 ]); close(pipe1[ 1 ]);
     close(pipe2[ 1 ]);
     close(pipe3[ 0 ]);
+    close(pipe4[ 0 ]); close(pipe4[ 1 ]);
+    
+    char p_buffer[256];
+    int line = 0;
+    while( read(pipe2[0], p_buffer, sizeof(p_buffer)+1) > 0 ){
+      char output_buffer[256];
+      sprintf(output_buffer, "%d. %s(%d)", line++, p_buffer, getpid());
+      write(pipe3[1], output_buffer, strlen(output_buffer)+1);
+    }
+    
+    close(pipe2[ 0 ]);
     close(pipe3[ 1 ]);
-    close(pipe4[ 0 ]);
-    close(pipe4[ 1 ]);
-
-
     exit(0);
   }
   if (fork() == 0){
-    close(pipe1[ 0 ]);
-    close(pipe1[ 1 ]);
-    close(pipe2[ 0 ]);
-    close(pipe2[ 1 ]);
-    close(pipe3[ 0 ]);
+    close(pipe1[ 0 ]); close(pipe1[ 1 ]);
+    close(pipe2[ 0 ]); close(pipe2[ 1 ]);
     close(pipe3[ 1 ]);
     close(pipe4[ 0 ]);
+    
+    char p_buffer[256];
+    while( read(pipe3[0], p_buffer, sizeof(p_buffer)+1) > 0 ){
+      char output_buffer[256];
+
+      int line;
+      char name[64];
+      int pid1, pid2, pid3;
+
+      sscanf(p_buffer, "%d. %s (%d)(%d)(%d)", &line, name, &pid1, &pid2, &pid3);
+      
+      if (strlen(name) < atoi(argv[2])){
+        sprintf(output_buffer, "%s(%d)\t|", p_buffer, getpid());
+        printf("%s\n", output_buffer);
+      } else {
+        sprintf(output_buffer, "%s(%d)", p_buffer, getpid());
+        write( pipe4[1], output_buffer, strlen(output_buffer) + 1);
+      }
+    }
+
+    
+    close(pipe3[ 0 ]);
+    close(pipe4[ 1 ]);
+    exit(0);
+  }
+  if (fork() == 0){
+    close(pipe1[ 0 ]); close(pipe1[ 1 ]);
+    close(pipe2[ 0 ]); close(pipe2[ 1 ]);
+    close(pipe3[ 1 ]);
     close(pipe4[ 1 ]);
 
+    char p_buffer[256];
+    while ( read(pipe4[0], p_buffer, sizeof(p_buffer)+1) > 0){
+      char output_buffer[256];
 
+      
+      for (int i=0; i<5; i++){
+        printf("\t");
+      }
+      sprintf(output_buffer, "| %s(%d)\n", p_buffer, getpid());
+      printf("%s", output_buffer);
+    }
+
+    close(pipe4[ 1 ]);
     exit(0);
   }
 
@@ -106,19 +137,12 @@ int main(int argc, char *argv[]) {
     usleep(1000000 / 20);
   }
 
-  close(pipe1[ 0 ]);
-  close(pipe1[ 1 ]);
-  close(pipe2[ 0 ]);
-  close(pipe2[ 1 ]);
-  close(pipe3[ 0 ]);
-  close(pipe3[ 1 ]);
-  close(pipe4[ 0 ]);
-  close(pipe4[ 1 ]);
+  close(pipe1[ 0 ]); close(pipe1[ 1 ]);
+  close(pipe2[ 0 ]); close(pipe2[ 1 ]);
+  close(pipe3[ 0 ]); close(pipe3[ 1 ]);
+  close(pipe4[ 0 ]); close(pipe4[ 1 ]);
   fclose(names);
-  wait(NULL);
-  wait(NULL);
-  wait(NULL);
-  wait(NULL);
+  wait(NULL); wait(NULL); wait(NULL); wait(NULL);
 
   return 0;
 }
